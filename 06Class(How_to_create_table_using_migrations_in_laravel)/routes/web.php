3eda9119b1a16f7064a54779d39961c74ef4de0d
AhsanHayat7 <ahsanhayat9071@gmail.com>
2024-03-15 01:44:25 +0500

    What is Modal in laravel and how to create it?, connecting modal with table

--- 06Class(How_to_create_table_using_migrations_in_laravel)/routes/web.php
+++ 06Class(How_to_create_table_using_migrations_in_laravel)/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Customers;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/customers', function(){
+    $customers = Customers::all();
+    echo "<pre>";
+    print_r($customers->toArray());
+
 });

4c70e1d7baaa9bb69a53c198d1ca0693ff3d5885
AhsanHayat7 <ahsanhayat9071@gmail.com>
2024-05-29 00:55:31 +0500

    Todo app dymanic this app with laravel php and mysql.

--- Todos-app/public/JS/main.js
+++ Todos-app/public/JS/main.js
@@ -1,55 +1,59 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+// Function to send AJAX request to delete todo item
+function deleteTodoItem(todoId) {
+    // Send a DELETE request to the server
+    fetch('/todos/' + todoId, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      // Reload the page after successful deletion
+      window.location.reload();
+    })
+    .catch(error => console.error('%cError: ' + error.message, 'color: black')); // Style error message with black color
 }
 
-// Click on a close button to hide the current list item
+// Event listener for clicking on close buttons
 var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
+for (var i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        // Get the todo item's ID from the parent element's data-id attribute
+        var todoId = div.getAttribute('data-id');
+        // Call the deleteTodoItem function with the todo ID
+        deleteTodoItem(todoId);
+    }
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
+// Function to create a new list item
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+    var inputValue = document.getElementById("myInput").value.trim(); // Trim whitespace
+    // Check if input value is empty
+    if (inputValue === '') {
+        // Alert message with black text on a white background
+        alert("Please enter a title!");
+        return;
     }
-  }
+    // Send a POST request to add the new todo item to the database
+    fetch('/todos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        },
+        body: JSON.stringify({ title: inputValue })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        // Reload the page after successful addition
+        window.location.reload();
+    })
+    .catch(error => console.error('%cError: ' + error.message, 'color: black')); // Style error message with black color
 }
--- Todos-app/resources/views/todos/index.blade.php
+++ Todos-app/resources/views/todos/index.blade.php
@@ -3,6 +3,17 @@
 @section('main-container')
 <div id="myDIV" class="header">
     <h2>My To Do List</h2>
+    <!-- Display validation errors, if any -->
+    @if ($errors->any())
+        <div class="alert alert-danger" style="color: black;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <!-- Form for creating new todo items -->
     <form action="{{ route('todos.store') }}" method="POST">
         @csrf
         <input type="text" id="myInput" name="title" placeholder="Title...">
@@ -11,14 +22,17 @@
 </div>
 
 <ul id="myUL">
+    <!-- Display existing todo items -->
     @foreach ($todos as $todo)
         <li>
             {{ $todo->title }}
+            <!-- Form for deleting todo items -->
             <form action="{{ route('todos.destroy', $todo->id) }}" method="POST" style="display:inline;">
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="close">&times;</button>
             </form>
+            <!-- Link for editing todo items -->
             <a href="{{ route('todos.edit', $todo->id) }}" class="edit">&#9998;</a>
         </li>
     @endforeach
